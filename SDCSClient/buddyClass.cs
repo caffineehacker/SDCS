48da4593160192c89ed6b99bf62bfcb5be9d4984
scott <scott@53c3396b-b429-0410-a041-aa3e8f182853>
2007-02-11 20:18:22 +0000

    Added the buddyClass, for representing each buddy on the client end

---
+++ SDCSClient/buddyClass.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Client
+{
+	/// <summary>
+	/// Summary description for buddyClass.
+	/// </summary>
+	public class buddy
+	{
+		public buddy()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+		
+		/// <summary>
+		/// This is the screen name/handle/username for the buddy.
+		/// </summary>
+		string handle = new string();
+		
+		/// <summary>
+		/// This is the numerical buddy Id.
+		/// </summary>
+		int buddyId = 0;
+
+		
+	}
+}
