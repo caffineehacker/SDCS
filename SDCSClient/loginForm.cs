a0750d429894ab384fe5f6724706dd52483f0432
Scott <Scott@53c3396b-b429-0410-a041-aa3e8f182853>
2007-02-27 23:47:04 +0000

    Pimping out the LoginForm

--- SDCSClient/loginForm.cs
+++ SDCSClient/loginForm.cs
@@ -18,6 +18,12 @@ namespace Client
 		private System.Windows.Forms.Label UsernameLabel;
 		private System.Windows.Forms.TextBox passwordTextBox;
 		private System.Windows.Forms.Label PasswordLabel;
+		private System.Windows.Forms.MainMenu mainMenu1;
+		private System.Windows.Forms.MenuItem menuItem1;
+		private System.Windows.Forms.MenuItem menuItem2;
+		private System.Windows.Forms.MenuItem menuItem3;
+		private System.Windows.Forms.MenuItem menuItem4;
+		private System.Windows.Forms.StatusBar loginFormStatusBar;
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -63,6 +69,12 @@ namespace Client
 			this.UsernameLabel = new System.Windows.Forms.Label();
 			this.passwordTextBox = new System.Windows.Forms.TextBox();
 			this.PasswordLabel = new System.Windows.Forms.Label();
+			this.mainMenu1 = new System.Windows.Forms.MainMenu();
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.menuItem4 = new System.Windows.Forms.MenuItem();
+			this.loginFormStatusBar = new System.Windows.Forms.StatusBar();
 			this.SuspendLayout();
 			// 
 			// usernameTextBox
@@ -119,19 +131,57 @@ namespace Client
 			this.PasswordLabel.TabIndex = 6;
 			this.PasswordLabel.Text = "Password:";
 			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																					  this.menuItem1});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																					  this.menuItem3,
+																					  this.menuItem4,
+																					  this.menuItem2});
+			this.menuItem1.Text = "Options";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 2;
+			this.menuItem2.Text = "Exit";
+			this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 0;
+			this.menuItem3.Text = "Choose Server";
+			// 
+			// menuItem4
+			// 
+			this.menuItem4.Index = 1;
+			this.menuItem4.Text = "Preferences";
+			// 
+			// loginFormStatusBar
+			// 
+			this.loginFormStatusBar.Location = new System.Drawing.Point(0, 131);
+			this.loginFormStatusBar.Name = "loginFormStatusBar";
+			this.loginFormStatusBar.Size = new System.Drawing.Size(322, 24);
+			this.loginFormStatusBar.TabIndex = 7;
+			// 
 			// LoginForm
 			// 
-			this.AcceptButton = this.loginButton;
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(320, 133);
+			this.ClientSize = new System.Drawing.Size(322, 155);
+			this.Controls.Add(this.loginFormStatusBar);
 			this.Controls.Add(this.PasswordLabel);
 			this.Controls.Add(this.passwordTextBox);
 			this.Controls.Add(this.UsernameLabel);
 			this.Controls.Add(this.loginButton);
 			this.Controls.Add(this.cancelButton);
 			this.Controls.Add(this.usernameTextBox);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
+			this.Menu = this.mainMenu1;
 			this.Name = "LoginForm";
 			this.Text = "SDCS - Login";
 			this.ResumeLayout(false);
@@ -150,6 +200,11 @@ namespace Client
 		private void cancelButton_Click(object sender, System.EventArgs e)
 		{
 			this.DialogResult = DialogResult.Cancel;
+		}
+
+		private void menuItem2_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.Application.Exit();
 		}
 	}
 }
